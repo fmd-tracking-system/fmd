5cecc08d8b5a1cd65fe05c1e3affc00504ea2586
ArtyomMinsk <artem.nesterenko@gmail.com>
2017-02-14 00:47:59 -0500

    Created models

--- fmd_track/fmd/models.py
+++ fmd_track/fmd/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Farm(models.Model):
+    farm_id = models.IntegerField()
+    name = models.CharField(max_length=300)
+    address = models.TextField()
+
+
+class FmdData(models.Model):
+    name = models.CharField(max_length=300)
+    fmd = models.ForeignKey(Farm)
+
+    date_observed = models.DateField()
+    time_observed = models.TimeField()
+
+    farm_condition = models.TextField()
+    bgp_requirements = models.TextField()
+    corrective_action = models.TextField()
+
+    hand_delivered_date = models.DateField()
+    hand_delivered_time = models.TimeField()
+
+    corrected_date = models.DateField()
+    corrected_time = models.TimeField()
